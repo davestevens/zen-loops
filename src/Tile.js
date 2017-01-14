f62c8f73d6d883cb8bc547fedeac19a8ac17dda3
Dave Stevens <dave@dstvns.co.uk>
2017-01-14 13:05:56 +0000

    Defines Tile class.

---
+++ src/Tile.js
@@ -0,0 +1,34 @@
+"use strict";
+
+"use strict";
+
+class Tile {
+  constructor({ top, right, bottom, left }) {
+    this.sides = [top, right, bottom, left];
+    this.rotation = 0;
+  }
+
+  get rotation() { return this._rotation; }
+  set rotation(value) {
+    this._rotation = (this._rotation || 0) + value;
+    const side = (current) => (((current - this.rotation) % 4) + 4) % 4;
+    this.top = this.sides[ side(0) ];
+    this.right = this.sides[ side(1) ];
+    this.bottom = this.sides[ side(2) ];
+    this.left = this.sides[ side(3) ];
+  }
+
+  get top() { return this._top; }
+  set top(value) { this._top = value; }
+
+  get right() { return this._right; }
+  set right(value) { this._right = value; }
+
+  get bottom() { return this._bottom; }
+  set bottom(value) { this._bottom = value; }
+
+  get left() { return this._left; }
+  set left(value) { this._left = value; }
+}
+
+export default Tile;
---
+++ test/Tile.test.js
@@ -0,0 +1,62 @@
+"use strict";
+
+import Tile from "../src/Tile";
+
+describe("Tile", () => {
+  describe("Rotation", () => {
+    context("when rotating clockwise", () => {
+      it("updates the Tile's sides", () => {
+        const tile = new Tile({
+          top: 0,
+          right: 1,
+          bottom: 2,
+          left: 3
+        });
+
+        tile.rotation = 1;
+
+        expect(tile.top).to.equal(3);
+        expect(tile.right).to.equal(0);
+        expect(tile.bottom).to.equal(1);
+        expect(tile.left).to.equal(2);
+      });
+    });
+
+    context("when rotating counterclockwise", () => {
+      it("updates the Tile's sides", () => {
+        const tile = new Tile({
+          top: 0,
+          right: 1,
+          bottom: 2,
+          left: 3
+        });
+
+        tile.rotation = -1;
+
+        expect(tile.top).to.equal(1);
+        expect(tile.right).to.equal(2);
+        expect(tile.bottom).to.equal(3);
+        expect(tile.left).to.equal(0);
+      });
+    });
+
+    context("when rotating multiple times", () => {
+      it("updates the Tile's sides", () => {
+        const tile = new Tile({
+          top: 0,
+          right: 1,
+          bottom: 2,
+          left: 3
+        });
+
+        tile.rotation = 3;
+        tile.rotation = -1;
+
+        expect(tile.top).to.equal(2);
+        expect(tile.right).to.equal(3);
+        expect(tile.bottom).to.equal(0);
+        expect(tile.left).to.equal(1);
+      });
+    });
+  });
+});
