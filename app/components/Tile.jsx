4ee16273f718d59d668c4472c78a00c106a075f2
Dave Stevens <dave@dstvns.co.uk>
2017-01-15 10:58:52 +0000

    Animates Tile rotation.

--- app/components/Tile.jsx
+++ app/components/Tile.jsx
@@ -1,12 +1,35 @@
 "use strict";
 
 import React, { Component, PropTypes } from "react";
+import { findDOMNode } from "react-dom";
 import { connect } from "react-redux";
 import { rotateTile } from "../actions/game";
 import classNames from "classnames";
 import "./Tile.scss";
 
+const ROTATIONS = {
+  "0-1": { from: "0", to: "90" },
+  "1-2": { from: "90", to: "180" },
+  "2-3": { from: "180", to: "270" },
+  "3-0": { from: "270", to: "360" }
+};
+
 class Tile extends Component {
+  componentWillUpdate(nextProps) {
+    const { rotation } = this.props;
+    const nextRotation = nextProps.rotation
+    const rotationAnimation = ROTATIONS[`${ rotation }-${ nextRotation }`];
+    const node = findDOMNode(this);
+
+    node.style.transform = `rotate(${ rotationAnimation.from }deg)`;
+    node.style.transition = 'transform 0s';
+
+    requestAnimationFrame(() => {
+      node.style.transform = `rotate(${ rotationAnimation.to }deg)`;
+      node.style.transition = 'transform 0.3s';
+    });
+  }
+
   render() {
     const { kind, rotation, size, x, y } = this.props;
 
