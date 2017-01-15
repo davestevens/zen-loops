1f211b66bf16166a04e0d9e43ded49d64a38bee9
Dave Stevens <dave@dstvns.co.uk>
2017-01-15 12:39:03 +0000

    Includes production build setup.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,23 +1,23 @@
 "use strict";
 
 const path = require("path");
-const webpack = require("webpack")
+const webpack = require("webpack");
 
 module.exports = {
-  devtool: "cheap-module-eval-source-map",
   entry: [
-    "webpack/hot/dev-server",
     "babel-polyfill",
     "./app/main.jsx"
   ],
   output: {
     path: path.join(__dirname, "build"),
-    publicPath: "/",
     filename: "bundle.js"
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify(process.env.NODE_ENV || "production")
+      }
+    })
   ],
   module: {
     loaders: [{
