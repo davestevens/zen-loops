0b1b9cde7dc8faddf952fb5141733742277b6c60
Dave Stevens <dave@dstvns.co.uk>
2017-01-17 20:45:15 +0000

    Performs rotation animation in CSS.

--- app/components/Tile.jsx
+++ app/components/Tile.jsx
@@ -1,45 +1,34 @@
 "use strict";
 
 import React, { Component, PropTypes } from "react";
-import { findDOMNode } from "react-dom";
 import { connect } from "react-redux";
 import { rotateTile } from "../actions/game";
 import classNames from "classnames";
 import "./Tile.scss";
 
-const ROTATIONS = [0, 90, 180, 270];
+const isForward = (current, next) => {
+  const diff = current - next;
+  return (diff == -1 || diff > 1);
+}
 
 class Tile extends Component {
   componentWillUpdate(nextProps) {
     const { rotation } = this.props;
     const nextRotation = nextProps.rotation
 
-    let from = ROTATIONS[rotation];
-    let to = ROTATIONS[nextRotation];
-    if ((rotation > nextRotation) && (rotation == ROTATIONS.length - 1)) {
-      to = 360 - to;
-    }
-    else if ((rotation < nextRotation) && (nextRotation == ROTATIONS.length - 1)) {
-      from = 360 - from;
-    }
-
-    const node = findDOMNode(this);
-    node.style.transform = `rotate(${ from }deg)`;
-    node.style.transition = 'transform 0s';
-
-    requestAnimationFrame(() => {
-      node.style.transform = `rotate(${ to }deg)`;
-      node.style.transition = 'transform 0.2s';
-    });
+    this.forward = isForward(rotation, nextRotation);
   }
 
   render() {
     const { kind, rotation, size, x, y } = this.props;
 
+    let obj = {};
+    obj[`tile--rotation-${ rotation }-reverse`] = !this.forward;
     const classnames = classNames(
       "tile",
       `tile--${ kind }`,
-      `tile--rotation-${ rotation }`
+      `tile--rotation-${ rotation }`,
+      obj
     );
 
     return (
